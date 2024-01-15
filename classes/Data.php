436edd461b43eff437bd8f1dbd7d7699515d6427
Kelsey De Busschere <kelseydebusschere@gmail.com>
2024-01-15 11:10:17 +0100

    Add form to view.php

--- classes/Data.php
+++ classes/Data.php
@@ -2,8 +2,6 @@
 
 class Data
 {
-    // This will provide the data you need
-    // No need to change any logic here - only data (to add or update words)
     public static function words(): array
     {
         return [
--- view.php
+++ view.php
@@ -2,12 +2,32 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<meta name="viewport"
-		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Game</title>
+	<title>LanguageGame</title>
 </head>
 <body>
-	<!-- TODO: add a form for the user to play the game -->
+	<header>
+		<h1>Language Quiz</h1>
+		<p>Translate the word to English!</p>
+	</header>
+	<main>
+		<!-- Put a random word from list here for the user to translate below -->
+		<!-- <p>word to translate</p> -->
+		<form action= "" method="POST" id="wordForm">
+			<input class="answer" id="answer" type="text" name="answer">
+			<br>
+			<input type="submit" value="Check">
+			
+		</form>
+		<section>
+			<!-- show if correct or not -->
+			<!-- add reset button -->
+		</section>
+
+	</main>
+	<footer>
+		Made by Kelsey for study purposes.
+	</footer>
 </body>
 </html>
